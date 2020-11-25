2319d241834677968e209db8fca2d8e38a3ea010
tGodson <tendongze95@gmail.com>
2020-11-25 17:29:18 +0100

    add basic components

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Display from './Display';
+import ButtonPanel from './ButtonPanel';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Display />
+      <ButtonPanel />
+    </React.Fragment>
+  );
+}
+
+export default App;
